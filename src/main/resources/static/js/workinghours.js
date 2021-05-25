40e1ac6305a622925d4c18ccdd92f241983ee60b
VILuave <zxmxwqxlove92595@163.com>
2021-05-25 16:01:13 +0800

    1.修改列表部分 新增toolbar 2.调整新增编辑页面样式

--- src/main/resources/static/js/workinghours.js
+++ src/main/resources/static/js/workinghours.js
@@ -1,9 +1,11 @@
 /**
+ * 使用jquery datatable插件构建工时管理页面.
+ *
  * Created by goldendba@gmail.com on 2017/3/8.
  */
 $(function () {
     var datatables_options = {
-        "dom": '<"toolbar">frtip',
+        "dom": "<'row'<'#toolbar.col-xs-2'l><'#params.col-xs-4'><'col-xs-6'f>r>" + "t" + "<'row'<'col-xs-6'i><'col-xs-6'p>>",
         "bAutoWidth": true,
         "bPaginate": false,
         "sPaginationType": "full_numbers",
@@ -17,11 +19,19 @@ $(function () {
         "bProcessing": true,
         "bJQueryUI": false,
         "language": {
-            "lengthMenu": "每页 _MENU_ 条记录",
+            "lengthMenu": "_MENU_ 条记录每页",
             "zeroRecords": "没有找到记录",
             "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
             "infoEmpty": "无记录",
-            "infoFiltered": "(从 _MAX_ 条记录过滤)"
+            "infoFiltered": "(从 _MAX_ 条记录过滤)",
+            "paginate": {
+                "previous": "上一页",
+                "next": "下一页"
+            }
+        },
+        initComplete: function () {
+            $("#toolbar").append('<button id="addBtn" type="button" class="btn btn-primary btn-sm">增加</button>&nbsp;');
+            $("#toolbar").append('<button id="delBtn" type="button" class="btn btn-primary btn-sm">删除</button>&nbsp');
         }
     };
 
@@ -32,15 +42,35 @@ $(function () {
 
     var table = $("#example1").DataTable(datatables_options);
     new $.fn.dataTable.FixedColumns(table, {"iLeftColumns": 7});
-    $("div.toolbar").html('<b style="color:red">自定义文字、图片等等</b>');
 
-    /*$("#check-all").click(function(){
+    /**
+     * 新增选中以及删除事件.
+     */
+    $('#example1 tbody').on('click', 'tr', function () {
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+        }
+        else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+    });
+
+    $("#addBtn").click(function () {
+        window.location = '/wk/edit';
+    });
+
+    $('#delBtn').click(function () {
+        table.row('.selected').remove().draw(false);
+    });
+
+    /*$("#check-all").click(function () {
      var val = $(this).prop("checked");
-     $("input[type='checkbox']",".DTFC_Cloned").attr("checked",val);
-     $("#check-all").attr("checked",val);
-     if(val){
+     $("input[type='checkbox']", ".DTFC_Cloned").attr("checked", val);
+     $("#check-all").attr("checked", val);
+     if (val) {
      $(this).parent().addClass("checked");//这里需要注意的是js会自动加上一个class,checked,需要加上才有选中效果
-     }else{
+     } else {
      $(this).parent().removeClass("checked");
      }
      });*/
